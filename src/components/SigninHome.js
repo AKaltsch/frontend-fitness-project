7131b2c0093364bcc48488f5edbabdb30d6f3555
alec <kaltschmidalec@gmail.com>
2021-10-24 16:46:16 -0700

    functioning map with geolocation

--- src/components/SigninHome.js
+++ src/components/SigninHome.js
@@ -1,86 +1,52 @@
 import React from "react";
 import { useState } from "react";
 import { Router, Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
 import Signup from "./Signup";
+import "./login.css";
 
-
-function SigninHome({onLogin, signUp}) {
+function SigninHome({ onLogin, signUp }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleLogin(e){
-      e.preventDefault();
-      onLogin(username, password)
+  function handleLogin(e) {
+    e.preventDefault();
+    onLogin(username, password);
   }
- 
-
-//   const url = "https://localhost:3000";
-
-//   const history = useHistory()
-
-//   function onLogin(username, password) {
-//     fetch(`${url}/api/v1/login`, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Accept: "application/json",
-//       },
-//       body: JSON.stringify({
-//         user: {
-//           username: `${username}`,
-//           password: `${password}`,
-//         },
-//       }),
-//     }).then((response) => {
-//       console.log(response);
-//       if (response.ok) {
-//         response.json().then((data) => {
-//           console.log("hi" + data.jwt);
-//           setUser(data.user);
-//           setLoggedIn(true);
-//           localStorage.setItem("jwt", data.jwt);
-//           history.push('/home')
-//           console.log("signed in!!!!!!!!!!!!!!!!!!");
-//         });
-//       } else {
-//         console.log("wrong username/password");
-//       }
-//     });
-//   }
 
   return (
     <div>
       <h1>Welcome to WeFit!</h1>
-      <form onSubmit={handleLogin} className="SignIn">
-        Username:
-        <input
-          className="login-inputs"
-          type="text"
-          id="username"
-          placeholder=" Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+      <div className="div-login">
+        <form onSubmit={handleLogin} className="SignIn">
+          Username:
+          <input
+            className="login-inputs"
+            type="text"
+            id="username"
+            placeholder=" Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <br />
+          Password:
+          <input
+            className="login-inputs"
+            type="password"
+            id="password"
+            placeholder=" Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button type="submit">Login</button>
+        </form>
         <br />
-        Password:
-        <input
-          className="login-inputs"
-          type="password"
-          id="password"
-          placeholder=" Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <button type="submit">Login</button>
-      </form>
-      <br />
-      Not a Member Yet?
-      <Signup signUp={signUp}/>
-      {/* <Link className="signin-home-link" to="/signup">
+        Not a Member Yet?
+        <Signup signUp={signUp} />
+        {/* <Link className="signin-home-link" to="/signup">
         Signup
       </Link> */}
+      </div>
     </div>
   );
 }
--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -1,5 +1,9 @@
+import {useState} from 'react'
 
 function Workouts() {
+
+    const [exercises, setExercises] = useState([])
+
     return(
         <h1>Workouts</h1>
     )
